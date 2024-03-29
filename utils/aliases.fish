6d0870cff1caab983d2652347a4b946102133ae3
Rumen <rumenmitov@protonmail.com>
2024-03-29 21:02:57 +0100

    transitioning to nixvim + removing unneeded aliases

--- utils/aliases.fish
+++ utils/aliases.fish
@@ -1,5 +1,4 @@
-alias nv="nvim"
-alias tmux="TERM=xterm-256color /usr/bin/tmux"
+alias nv="nix run $HOME/.config/nixvim"
 alias stop="kill -9 $(lsof -i tcp:443 | grep LISTEN | awk '{print $2}')"
 alias tty-clock=" tty-clock -c -B -C 6"
 alias gac="git add . && git commit -m"
